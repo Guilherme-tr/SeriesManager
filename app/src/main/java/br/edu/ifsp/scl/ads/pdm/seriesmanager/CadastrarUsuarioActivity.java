de34a249dd7ae7230e394ccb3f68db7d056c50b7
Guilherme-tr <gui.trujilo.wecomp@gmail.com>
2021-12-01 22:14:02 -0300

    Telas de autenticação prontas

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/seriesmanager/CadastrarUsuarioActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.scl.ads.pdm.seriesmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastrarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_usuario);
+    }
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/seriesmanager/RecuperarSenhaActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.scl.ads.pdm.seriesmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuperarSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_senha);
+    }
+}
